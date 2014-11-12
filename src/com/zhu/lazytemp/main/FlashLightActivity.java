f35195076be15028c799ebf3456c95e73e196d70
DeshuaiZhu <LENOVO@DeshuaiZhu>
2014-11-12 17:36:37 +0800

    zhudeshuai：完成闪光灯功能(for Android 4.4)

--- src/com/zhu/lazytemp/main/FlashLightActivity.java
+++ src/com/zhu/lazytemp/main/FlashLightActivity.java
@@ -3,37 +3,31 @@ package com.zhu.lazytemp.main;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 import com.zhu.lazytemp.R;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
 
 /**
  * 开启闪光灯
  * @author deshuaizhu
  * @since 2014-9-3 17:51:29
  */
-public class FlashLightActivity extends Activity implements SurfaceHolder.Callback {
+public class FlashLightActivity extends Activity{
     /** 开关 */
     private ToggleButton tb_switch;
-    private Camera.Parameters parameters;
     private Camera m_Camera;
-    /** SurfaceView */
-    private SurfaceView surfaceview;
-    private SurfaceHolder holder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashlight);
+        init();
         initView();
 
     }
@@ -44,18 +38,31 @@ public class FlashLightActivity extends Activity implements SurfaceHolder.Callba
         {
             m_Camera = Camera.open();
             try {
-                m_Camera.setPreviewDisplay(holder);
+                //Android 4.4 必须要加上这样一句才可以打开闪光灯
+                m_Camera.setPreviewTexture(new SurfaceTexture(0));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
+    /**
+     * 检查设备是否有闪光灯
+     * @return
+     */
+    private boolean checkCameraHardware() {
+        boolean result = false;
+        PackageManager pm = getPackageManager();
+        if (pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)
+                && pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+            // this device has a camera and flash
+            result = true;
+        }
+        return result;
+    }
+
     private void initView() {
         tb_switch = (ToggleButton)findViewById(R.id.toggleButton);
-        surfaceview = (SurfaceView)findViewById(R.id.sv);
-        holder = surfaceview.getHolder();
-        holder.addCallback(this);
         tb_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
@@ -69,7 +76,6 @@ public class FlashLightActivity extends Activity implements SurfaceHolder.Callba
     }
 
     private void OpenLightOn()    {
-       init();
         Camera.Parameters parameters = m_Camera.getParameters();
         parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
         m_Camera.setParameters(parameters);
@@ -77,36 +83,11 @@ public class FlashLightActivity extends Activity implements SurfaceHolder.Callba
     }
 
     private void CloseLightOff()   {
-        if ( m_Camera != null )
-        {
-            m_Camera.stopPreview();
-            m_Camera.release();
-            m_Camera = null;
-        }
+        Camera.Parameters parameters = m_Camera.getParameters();
+        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        m_Camera.setParameters(parameters);
+        m_Camera.startPreview();
     }
-
-    /**
-     * 设置闪光灯的开启和关闭
-     * @param isEnable
-     * @author linc
-     * @date 2012-3-18
-     */
-   /* private void setFlashlightEnabled(boolean isEnable)
-    {
-        try
-        {
-            Method method = Class.forName("android.os.ServiceManager").getMethod("getService", String.class);
-            IBinder binder = (IBinder) method.invoke(null, new Object[] { "hardware" });
-
-            IHardwareService localhardwareservice = IHardwareService.Stub.asInterface(binder);
-            localhardwareservice.setFlashlightEnabled(isEnable);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
-    }*/
 
     /**
      * 开启打开闪光灯页面
@@ -116,34 +97,12 @@ public class FlashLightActivity extends Activity implements SurfaceHolder.Callba
         context.startActivity(new Intent(context,FlashLightActivity.class));
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-       init();
-       /* m_Camera.autoFocus( new Camera.AutoFocusCallback (){
-            public void onAutoFocus(boolean success, Camera camera) {
-            }
-        });*/
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        if ( m_Camera != null )
-        {
-            m_Camera.stopPreview();
-            m_Camera.release();
-            m_Camera = null;
-        }
-    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         if(m_Camera!=null){
+            m_Camera.release();
             m_Camera = null;
         }
     }
